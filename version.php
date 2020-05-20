f9fc910acda9be8b28c7eac1b3b82279404b2eb3
avegys <andrius.vegys@enovation.ie>
2020-05-20 12:38:55 +0300

    Changes for plugin to work with Moodle 3.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020032700;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051710;        // Requires this Moodle version.
+$plugin->version   = 2020032701;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2016052318;        // Requires this Moodle version.
 $plugin->component = 'atto_teamsmeeting';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;        // Maturity level of this plugin version
-$plugin->release  = 'v1.2';        // Requires this Moodle version.
+$plugin->release  = 'v1.2.1';        // Requires this Moodle version.
